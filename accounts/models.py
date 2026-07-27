64c0798085c08614d33c64a5cc28575f4351bab4
Nikki Walker <nwalker@generalrv.com>
2026-07-27 13:25:00 -0400

    miss staged file

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-<<<<<<< HEAD
 # Create your models here
-=======
 class Patient(models.Model):
 
     patient_id = models.AutoField(primary_key=True)
@@ -15,4 +13,3 @@ class Patient(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
->>>>>>> main
